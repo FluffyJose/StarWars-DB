b7a18ee7270a9751a9f0f9baa491ffcffb94dc71
FluffyJose <3oppbI4@gmail.com>
2019-10-16 00:15:51 +0300

    15.10.2019

---
+++ src/index.js
@@ -0,0 +1,17 @@
+
+const getResource =async (url) => {
+  const res = await fetch(url);
+  if (!res.ok) {
+    throw new Error(`Could not fetch ${url}, received ${res.status}`)
+  }
+  const body = await res.json();
+  return body;
+};
+
+getResource('https://swapi.co/api/people/123126/')
+.then((body) => {
+  console.log(body);
+})
+.catch((err) => {
+  console.log('Could not fetch', err);
+});
